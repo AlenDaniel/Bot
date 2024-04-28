83b9d3860d3b394a9b64b9a037a7b959633054ad
Daniel <danielghost@live.com>
2024-04-28 16:06:45 +0800

    fix: bug

---
+++ test/plugin.py
@@ -0,0 +1,31 @@
+"""template of your bot"""
+from __future__ import annotations
+import asyncio
+import os
+from wechaty import Wechaty, WechatyOptions
+
+from dotenv import load_dotenv
+# from wechaty_plugin_contrib.contrib.info_logger import InfoLoggerPlugin
+from src.plugins.ding_dong import DingDongPlugin
+from src.plugins.repeater import RepeaterPlugin
+from src.plugins.counter import CounterPlugin, UICounterPlugin
+from src.plugins.github_message_forwarder import GithubMessageForwarderPlugin
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    options = WechatyOptions(
+        port=os.environ.get('port', 8004)
+    )
+    bot = Wechaty(options)
+    bot.use([
+        DingDongPlugin(),
+        RepeaterPlugin(),
+        # InfoLoggerPlugin(),
+        CounterPlugin(),
+        UICounterPlugin(),
+        GithubMessageForwarderPlugin(
+            endpoint=os.environ.get('github_endpoint', None) or "your-custom-endpoint"
+        ),
+    ])
+    asyncio.run(bot.start())
